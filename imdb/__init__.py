78bd9f3bb551ad7d70b6ecbe97f95e89a7589179
alberanid <none@none>
2008-09-17 11:59:01 +0000

    - fixes for the new HTML of IMDb.
    --HG--
    extra : convert_revision : svn%3A5465fa6c-2427-4886-a692-964263225485/trunk/imdbpy%40387

--- imdb/__init__.py
+++ imdb/__init__.py
@@ -25,7 +25,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 __all__ = ['IMDb', 'IMDbError', 'Movie', 'Person', 'Character', 'Company',
             'available_access_systems']
-__version__ = VERSION = '3.7.cvs20080813'
+__version__ = VERSION = '3.7.cvs20080917'
 
 # Import compatibility module (importing it is enough).
 import _compat

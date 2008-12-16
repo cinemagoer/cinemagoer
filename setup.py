f08f70134dbf49363524247950e276f0d6b6f8d1
alberanid <none@none>
2008-12-16 15:38:21 +0000

    - another cosmetic change to test SVN (sorry, this is the last one!)
    --HG--
    extra : convert_revision : svn%3A5465fa6c-2427-4886-a692-964263225485/trunk/imdbpy%40447

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ DO_SCRIPTS = 1
 
 # version of the software; CVS releases contain a string
 # like ".cvsYearMonthDay(OptionalChar)".
-version = '3.9cvs20081216b'
+version = '3.9cvs20081216c'
 
 home_page = 'http://imdbpy.sf.net/'
 

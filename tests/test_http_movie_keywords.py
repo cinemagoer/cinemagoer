fc556e7dccd9339c1e729da400bc9f3f1aae163e
H. Turgut Uyar <uyar@tekir.org>
2017-11-10 19:26:50 +0300

    Fix movie keyword parser test names

--- tests/test_http_movie_keywords.py
+++ tests/test_http_movie_keywords.py
@@ -17,14 +17,14 @@ def movie_keywords(movies):
 parser = DOMHTMLKeywordsParser()
 
 
-def test_summary_should_end_with_author(movie_keywords):
+def test_keywords_multiple_should_be_parsed_correctly(movie_keywords):
     page = movie_keywords('matrix')
     data = parser.parse(page)['data']
     keywords = set(data['keywords'])
     assert {'computer-hacker', 'messiah', 'artificial-reality'}.issubset(keywords)
 
 
-def test_summary_none_should_be_excluded(movie_keywords):
+def test_keywords_none_should_be_excluded(movie_keywords):
     page = movie_keywords('ates parcasi')
     data = parser.parse(page)['data']
     assert 'keywords' not in data

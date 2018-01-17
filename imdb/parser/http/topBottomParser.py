97ae5763b0d456afa636e69748785e25038525a7
H. Turgut Uyar <uyar@tekir.org>
2018-01-17 14:59:57 +0300

    Fix rank parsing in top/bottom parsers
    Legacy branch does not have tests

--- imdb/parser/http/topBottomParser.py
+++ imdb/parser/http/topBottomParser.py
@@ -42,17 +42,24 @@ class DOMHTMLTop250Parser(DOMParserBase):
     ranktext = 'top 250 rank'
 
     def _init(self):
-        self.extractors = [Extractor(label=self.label,
-                        path="//div[@id='main']//div[1]//div//table//tbody//tr",
-                        attrs=Attribute(key=None,
-                                multi=True,
-                                path={self.ranktext: "./td[2]//text()",
-                                        'rating': "./td[3]//strong//text()",
-                                        'title': "./td[2]//a//text()",
-                                        'year': "./td[2]//span//text()",
-                                        'movieID': "./td[2]//a/@href",
-                                        'votes': "./td[3]//strong/@title"
-                                        }))]
+        self.extractors = [
+            Extractor(
+                label=self.label,
+                path="//div[@id='main']//div[1]//div//table//tbody//tr",
+                attrs=Attribute(
+                    key=None,
+                    multi=True,
+                    path={
+                        self.ranktext: "./td[2]/text()",
+                        'rating': "./td[3]//strong//text()",
+                        'title': "./td[2]//a//text()",
+                        'year': "./td[2]//span//text()",
+                        'movieID': "./td[2]//a/@href",
+                        'votes': "./td[3]//strong/@title"
+                    }
+                )
+            )
+        ]
 
     def postprocess_data(self, data):
         if not data or self.label not in data:

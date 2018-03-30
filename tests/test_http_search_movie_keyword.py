18a40372cf43c3e1911c6a137b6d26b1df01f296
H. Turgut Uyar <uyar@tekir.org>
2018-03-30 14:06:00 +0300

    added future imports to all test modules

--- tests/test_http_search_movie_keyword.py
+++ tests/test_http_search_movie_keyword.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
 from pytest import fixture, mark
 
 from imdb.parser.http.searchKeywordParser import DOMHTMLSearchMovieKeywordParser

6e3bc1a4dfc035812bfacc3032d5e5e1ca78421c
H. Turgut Uyar <uyar@tekir.org>
2018-04-09 14:37:44 +0300

    refactor movie sites tests

--- tests/test_http_movie_sites.py
+++ tests/test_http_movie_sites.py
@@ -1,4 +1,10 @@
-def test_sites_should_be_lists(ia):
-    data = ia.get_movie('0133093', info=['official sites'])     # Matrix
-    assert len(data.get('official sites', [])) == 1
-    assert len(data.get('sound clips', [])) == 3
+def test_movie_official_sites_should_be_a_list(ia):
+    movie = ia.get_movie('0133093', info=['official sites'])    # Matrix
+    official_sites = movie.get('official sites', [])
+    assert len(official_sites) == 1
+
+
+def test_movie_sound_clips_should_be_a_list(ia):
+    movie = ia.get_movie('0133093', info=['official sites'])    # Matrix
+    sound_clips = movie.get('sound clips', [])
+    assert len(sound_clips) == 3

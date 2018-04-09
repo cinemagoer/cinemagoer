dd9b2ed49924c46453167ee4fe662ce0de0b8b18
H. Turgut Uyar <uyar@tekir.org>
2018-04-09 10:53:30 +0300

    rewrite movie plot tests using access fixture

--- tests/test_http_movie_plot.py
+++ tests/test_http_movie_plot.py
@@ -1,35 +1,16 @@
-from pytest import fixture
-
 import re
 
-from imdb.parser.http.movieParser import DOMHTMLPlotParser
 
-
-@fixture(scope='module')
-def movie_plot_summaries(url_opener, movies):
-    """A function to retrieve the plot summary page of a test movie."""
-    def retrieve(movie_key):
-        url = movies[movie_key] + '/plotsummary'
-        return url_opener.retrieve_unicode(url)
-    return retrieve
-
-
-parser = DOMHTMLPlotParser()
-
-
-def test_summary_should_end_with_author(movie_plot_summaries):
-    page = movie_plot_summaries('matrix')
-    data = parser.parse(page)['data']
+def test_summary_should_end_with_author(ia):
+    data = ia.get_movie('0133093', info=['plot'])   # Matrix
     assert re.match('^Thomas A\. Anderson is a man .*::redcommander27$', data['plot'][0])
 
 
-def test_summary_should_have_synopsis(movie_plot_summaries):
-    page = movie_plot_summaries('matrix')
-    data = parser.parse(page)['data']
+def test_summary_should_have_synopsis(ia):
+    data = ia.get_movie('0133093', info=['plot'])   # Matrix
     assert len(data['synopsis']) >= 1 and data['synopsis'][0]
 
 
-def test_summary_none_should_be_excluded(movie_plot_summaries):
-    page = movie_plot_summaries('ates parcasi')
-    data = parser.parse(page)['data']
+def test_summary_none_should_be_excluded(ia):
+    data = ia.get_movie('1863157', info=['plot'])   # Ates Parcasi
     assert 'plot' not in data

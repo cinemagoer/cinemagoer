626809893de13f8d08e902cf1aba12a42bd05363
H. Turgut Uyar <uyar@tekir.org>
2018-04-09 15:00:58 +0300

    refactor movie votes tests

--- tests/test_http_movie_votes.py
+++ tests/test_http_movie_votes.py
@@ -1,30 +1,41 @@
-def test_movie_ratings_nr_votes_should_be_10(ia):
-    data = ia.get_movie('0063850', info=['vote details'])   # Matrix
-    assert len(data['number of votes']) == 10
+from pytest import mark
 
 
-def test_movie_ratings_nr_votes_should_be_integer(ia):
-    data = ia.get_movie('0063850', info=['vote details'])   # Matrix
-    assert isinstance(data['number of votes'][10], int)
+def test_movie_votes_should_be_divided_into_10_slots(ia):
+    movie = ia.get_movie('0133093', info=['vote details'])  # Matrix
+    votes = movie.get('number of votes', [])
+    assert len(votes) == 10
 
 
-def test_movie_ratings_median_should_integer(ia):
-    data = ia.get_movie('0063850', info=['vote details'])   # Matrix
-    assert isinstance(data['median'], int)
+def test_movie_votes_should_be_integers(ia):
+    movie = ia.get_movie('0133093', info=['vote details'])  # Matrix
+    votes = movie.get('number of votes', [])
+    for vote in votes:
+        assert isinstance(vote, int)
 
 
-def test_movie_ratings_mean_should_be_numeric(ia):
-    data = ia.get_movie('0063850', info=['vote details'])   # Matrix
-    assert isinstance(data['arithmetic mean'], float)
+@mark.fragile
+def test_movie_votes_median_should_be_an_integer(ia):
+    movie = ia.get_movie('0133093', info=['vote details'])  # Matrix
+    median = movie.get('median')
+    assert median == 9
 
 
-def test_movie_ratings_demographics_should_be_19(ia):
-    data = ia.get_movie('0063850', info=['vote details'])   # Matrix
-    assert len(data['demographics']) == 19
+@mark.fragile
+def test_movie_votes_mean_should_be_numeric(ia):
+    movie = ia.get_movie('0133093', info=['vote details'])  # Matrix
+    mean = movie.get('arithmetic mean')
+    assert 8.5 <= mean <= 9
 
 
-def test_movie_ratings_demographics_should_be_numeric(ia):
-    data = ia.get_movie('0063850', info=['vote details'])   # Matrix
-    top1000 = data['demographics']['top 1000 voters']
+def test_movie_demographics_should_be_divided_into_19_categories(ia):
+    movie = ia.get_movie('0133093', info=['vote details'])  # Matrix
+    demographics = movie['demographics']
+    assert len(demographics) == 19
+
+
+def test_movie_demographics_votes_should_be_integers(ia):
+    movie = ia.get_movie('0133093', info=['vote details'])  # Matrix
+    top1000 = movie['demographics']['top 1000 voters']
     assert isinstance(top1000['votes'], int)
     assert isinstance(top1000['rating'], float)

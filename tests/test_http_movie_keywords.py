e6c1da321c37fe46a6c5b4387811074ecc19e142
H. Turgut Uyar <uyar@tekir.org>
2018-04-09 14:30:48 +0300

    fix movie keywords test name

--- tests/test_http_movie_keywords.py
+++ tests/test_http_movie_keywords.py
@@ -9,6 +9,6 @@ def test_movie_keywords_should_be_a_list_of_keywords(ia):
     assert {'computer-hacker', 'messiah', 'artificial-reality'}.issubset(set(keywords))
 
 
-def test_keywords_none_should_be_excluded(ia):
+def test_movie_keywords_if_none_should_be_excluded(ia):
     movie = ia.get_movie('1863157', info=['keywords'])  # Ates Parcasi
     assert 'keywords' not in movie

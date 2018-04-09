dcabf497d52eea02cad45ecacf4fa51228346ee8
H. Turgut Uyar <uyar@tekir.org>
2018-04-09 11:19:10 +0300

    set infoset to main for company tests

--- tests/test_http_company_main.py
+++ tests/test_http_company_main.py
@@ -1,3 +1,3 @@
 def test_name_should_not_include_country(ia):
-    data = ia.get_company('0017902')
+    data = ia.get_company('0017902', info=['main'])
     assert data['name'] == 'Pixar Animation Studios'

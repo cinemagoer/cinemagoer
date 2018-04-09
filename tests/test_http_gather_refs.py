f61854d82692bd9b48580016f74f0eacc0f4f2c9
H. Turgut Uyar <uyar@tekir.org>
2018-04-09 14:33:58 +0300

    refactor gather refs tests

--- tests/test_http_gather_refs.py
+++ tests/test_http_gather_refs.py
@@ -1,4 +1,4 @@
-def test_movie_and_person_refs_should_be_lists(ia):
-    data = ia.get_person('0000210', info=['biography'])     # Julia Roberts
-    assert len(data.namesRefs) > 100
-    assert len(data.titlesRefs) > 70
+def test_references_to_titles_and_names_should_be_lists(ia):
+    person = ia.get_person('0000210', info=['biography'])   # Julia Roberts
+    assert len(person.get_namesRefs()) > 100
+    assert len(person.get_titlesRefs()) > 70

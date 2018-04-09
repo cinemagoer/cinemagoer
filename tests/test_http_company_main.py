5eeb9d6ae2148562a3919fe3e608e125cc2760cf
H. Turgut Uyar <uyar@tekir.org>
2018-04-09 14:16:54 +0300

    refactor company test

--- tests/test_http_company_main.py
+++ tests/test_http_company_main.py
@@ -1,3 +1,3 @@
-def test_name_should_not_include_country(ia):
+def test_company_name_should_not_include_country(ia):
     data = ia.get_company('0017902', info=['main'])
-    assert data['name'] == 'Pixar Animation Studios'
+    assert data.get('name') == 'Pixar Animation Studios'

96e235e86d72be9440254ff71a39d124b518827e
H. Turgut Uyar <uyar@tekir.org>
2018-04-09 14:20:24 +0300

    refactor movie keywords tests

--- tests/test_http_movie_keywords.py
+++ tests/test_http_movie_keywords.py
@@ -1,8 +1,14 @@
-def test_keywords_multiple_should_be_parsed_correctly(ia):
-    data = ia.get_movie('0133093', info=['keywords'])   # Matrix
-    assert {'computer-hacker', 'messiah', 'artificial-reality'}.issubset(set(data['keywords']))
+from pytest import mark
+
+
+@mark.fragile
+def test_movie_keywords_should_be_a_list_of_keywords(ia):
+    movie = ia.get_movie('0133093', info=['keywords'])  # Matrix
+    keywords = movie.get('keywords', [])
+    assert 250 <= len(keywords) <= 400
+    assert {'computer-hacker', 'messiah', 'artificial-reality'}.issubset(set(keywords))
 
 
 def test_keywords_none_should_be_excluded(ia):
-    data = ia.get_movie('1863157', info=['keywords'])   # Ates Parcasi
-    assert 'keywords' not in data
+    movie = ia.get_movie('1863157', info=['keywords'])  # Ates Parcasi
+    assert 'keywords' not in movie

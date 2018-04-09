813bf4841a7fd93f6aa4f03496b28c43dd20cc8d
H. Turgut Uyar <uyar@tekir.org>
2018-04-09 10:56:05 +0300

    rewrite movie reviews tests using access fixture

--- tests/test_http_movie_reviews.py
+++ tests/test_http_movie_reviews.py
@@ -1,21 +1,3 @@
-from pytest import fixture
-
-from imdb.parser.http.movieParser import DOMHTMLReviewsParser
-
-
-@fixture(scope='module')
-def movie_reviews(url_opener, movies):
-    """A function to retrieve the reviews page of a test movie."""
-    def retrieve(movie_key):
-        url = movies[movie_key] + '/reviews'
-        return url_opener.retrieve_unicode(url)
-    return retrieve
-
-
-parser = DOMHTMLReviewsParser()
-
-
-def test_number_of_reviews_should_be_correct(movie_reviews):
-    page = movie_reviews('dust devil')
-    data = parser.parse(page)['data']
+def test_number_of_reviews_should_be_correct(ia):
+    data = ia.get_movie('0104155', info=['reviews'])    # Dust Devil
     assert len(data.get('reviews', [])) == 25

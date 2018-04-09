2cddf22da899701c02ad783260a4679ea6da7c72
H. Turgut Uyar <uyar@tekir.org>
2018-04-09 15:47:11 +0300

    separate names and titles refs tests

--- tests/test_http_gather_refs.py
+++ tests/test_http_gather_refs.py
@@ -1,4 +1,8 @@
-def test_references_to_titles_and_names_should_be_lists(ia):
+def test_references_to_titles_should_be_a_list(ia):
     person = ia.get_person('0000210', info=['biography'])   # Julia Roberts
-    assert len(person.get_namesRefs()) > 100
-    assert len(person.get_titlesRefs()) > 70
+    assert 70 < len(person.get_titlesRefs()) < 100
+
+
+def test_references_to_names_should_be_a_list(ia):
+    person = ia.get_person('0000210', info=['biography'])  # Julia Roberts
+    assert 100 < len(person.get_namesRefs()) < 150

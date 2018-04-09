ac6f891c0a69141c5cff482851ee2e306359ae4c
H. Turgut Uyar <uyar@tekir.org>
2018-04-09 14:44:06 +0300

    refactor movie taglines tests

--- tests/test_http_movie_taglines.py
+++ tests/test_http_movie_taglines.py
@@ -1,14 +1,16 @@
-def test_movie_taglines_single_should_be_a_list_of_phrases(ia):
-    data = ia.get_movie('0063850', info=['taglines'])   # If....
-    assert data['taglines'] == ['Which side will you be on?']
+def test_movie_taglines_if_single_should_be_a_list_of_phrases(ia):
+    movie = ia.get_movie('0063850', info=['taglines'])  # If....
+    taglines = movie.get('taglines', [])
+    assert taglines == ['Which side will you be on?']
 
 
-def test_movie_taglines_multiple_should_be_a_list_of_phrases(ia):
-    data = ia.get_movie('0060666', info=['taglines'])   # Manos
-    assert len(data['taglines']) == 3
-    assert data['taglines'][0] == "It's Shocking! It's Beyond Your Imagination!"
+def test_movie_taglines_if_multiple_should_be_a_list_of_phrases(ia):
+    movie = ia.get_movie('0060666', info=['taglines'])  # Manos
+    taglines = movie.get('taglines', [])
+    assert len(taglines) == 3
+    assert taglines[0] == "It's Shocking! It's Beyond Your Imagination!"
 
 
-def test_summary_none_should_be_excluded(ia):
-    data = ia.get_movie('1863157', info=['taglines'])   # Ates Parcasi
-    assert 'taglines' not in data
+def test_movie_taglines_if_none_should_be_excluded(ia):
+    movie = ia.get_movie('1863157', info=['taglines'])  # Ates Parcasi
+    assert 'taglines' not in movie

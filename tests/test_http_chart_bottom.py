2765c2b4cfe1d63ae3fffc53344b73f13fabdf30
H. Turgut Uyar <uyar@tekir.org>
2018-04-09 13:41:02 +0300

    add tests for bottom 100 movies

--- tests/test_http_chart_bottom.py
+++ tests/test_http_chart_bottom.py
@@ -1,3 +1,6 @@
+from pytest import mark
+
+
 def test_bottom_chart_should_contain_100_movies(ia):
     data = ia.get_bottom100_movies()
     assert len(data) == 100
@@ -6,10 +9,27 @@ def test_bottom_chart_should_contain_100_movies(ia):
 def test_bottom_chart_all_movies_should_have_rating_and_votes(ia):
     data = ia.get_bottom100_movies()
     for movie in data:
-        assert {'title', 'kind', 'year', 'rating', 'votes'}.issubset(set(movie.keys()))
+        assert {'bottom 100 rank', 'title', 'kind', 'year', 'rating', 'votes'}.issubset(set(movie.keys()))
+
+
+def test_bottom_chart_ranks_should_proceed_in_order(ia):
+    data = ia.get_top250_movies()
+    ranks = [m['bottom 100 rank'] for m in data]
+    assert ranks == list(range(1, 101))
 
 
 def test_bottom_chart_should_contain_manos(ia):
     data = ia.get_bottom100_movies()
     movieIDs = [m.movieID for m in data]
     assert '0060666' in movieIDs
+
+
+@mark.fragile
+def test_bottom_chart_koz_should_be_bottom_movie(ia):
+    data = ia.get_bottom100_movies()
+    assert data[0].movieID == '4458206'
+    assert data[0]['title'] == 'Code Name: K.O.Z.'
+    assert data[0]['kind'] == 'movie'
+    assert data[0]['year'] == 2015
+    assert data[0]['rating'] < 1.6
+    assert data[0]['votes'] > 25000
--- tests/test_http_chart_top.py
+++ tests/test_http_chart_top.py
@@ -1,3 +1,6 @@
+from pytest import mark
+
+
 def test_top_chart_should_contain_250_movies(ia):
     data = ia.get_top250_movies()
     assert len(data) == 250
@@ -6,7 +9,7 @@ def test_top_chart_should_contain_250_movies(ia):
 def test_top_chart_all_movies_should_have_rating_and_votes(ia):
     data = ia.get_top250_movies()
     for movie in data:
-        assert {'title', 'kind', 'year', 'rating', 'votes'}.issubset(set(movie.keys()))
+        assert {'top 250 rank', 'title', 'kind', 'year', 'rating', 'votes'}.issubset(set(movie.keys()))
 
 
 def test_top_chart_ranks_should_proceed_in_order(ia):
@@ -21,11 +24,12 @@ def test_top_chart_should_contain_matrix(ia):
     assert '0133093' in movieIDs
 
 
+@mark.fragile
 def test_top_chart_shawshank_should_be_top_movie(ia):
     data = ia.get_top250_movies()
-    top_movie = data[0]
-    assert top_movie.movieID == '0111161'
-    assert top_movie['title'] == 'The Shawshank Redemption'
-    assert top_movie['year'] == 1994
-    assert top_movie['rating'] > 9
-    assert top_movie['votes'] > 1900000
+    assert data[0].movieID == '0111161'
+    assert data[0]['title'] == 'The Shawshank Redemption'
+    assert data[0]['kind'] == 'movie'
+    assert data[0]['year'] == 1994
+    assert data[0]['rating'] > 9
+    assert data[0]['votes'] > 1900000

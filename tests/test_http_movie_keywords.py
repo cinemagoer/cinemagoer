cdfd8e72c5ee498aeeb1bfd160ba5f02ed224378
H. Turgut Uyar <uyar@tekir.org>
2018-04-09 10:50:32 +0300

    rewrite movie keywords tests using access fixture

--- tests/test_http_movie_keywords.py
+++ tests/test_http_movie_keywords.py
@@ -1,27 +1,8 @@
-from pytest import fixture
-
-from imdb.parser.http.movieParser import DOMHTMLKeywordsParser
-
-
-@fixture(scope='module')
-def movie_keywords(url_opener, movies):
-    """A function to retrieve the keywords page of a test movie."""
-    def retrieve(movie_key):
-        url = movies[movie_key] + '/keywords'
-        return url_opener.retrieve_unicode(url)
-    return retrieve
-
-
-parser = DOMHTMLKeywordsParser()
-
-
-def test_keywords_multiple_should_be_parsed_correctly(movie_keywords):
-    page = movie_keywords('matrix')
-    data = parser.parse(page)['data']
+def test_keywords_multiple_should_be_parsed_correctly(ia):
+    data = ia.get_movie('0133093', info=['keywords'])   # Matrix
     assert {'computer-hacker', 'messiah', 'artificial-reality'}.issubset(set(data['keywords']))
 
 
-def test_keywords_none_should_be_excluded(movie_keywords):
-    page = movie_keywords('ates parcasi')
-    data = parser.parse(page)['data']
+def test_keywords_none_should_be_excluded(ia):
+    data = ia.get_movie('1863157', info=['keywords'])   # Ates Parcasi
     assert 'keywords' not in data

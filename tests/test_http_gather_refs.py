7b9ef0455f0652b32d0fd961932a9c9f9c18ca85
H. Turgut Uyar <uyar@tekir.org>
2018-04-09 10:46:39 +0300

    rewrite gather refs tests using access fixture

--- tests/test_http_gather_refs.py
+++ tests/test_http_gather_refs.py
@@ -1,22 +1,4 @@
-from pytest import fixture
-
-from imdb.parser.http.utils import GatherRefs
-
-
-@fixture(scope='module')
-def person_bio(url_opener, people):
-    """A function to retrieve the main details page of a test person."""
-    def retrieve(person_key):
-        url = people[person_key] + '/bio'
-        return url_opener.retrieve_unicode(url)
-    return retrieve
-
-
-parser = GatherRefs()
-
-
-def test_movies_and_persons_refs_should_be_lists(person_bio):
-    page = person_bio('julia roberts')
-    data = parser.parse(page)
-    assert len(data['names refs']) > 100
-    assert len(data['titles refs']) > 70
+def test_movie_and_person_refs_should_be_lists(ia):
+    data = ia.get_person('0000210', info=['biography'])     # Julia Roberts
+    assert len(data.namesRefs) > 100
+    assert len(data.titlesRefs) > 70

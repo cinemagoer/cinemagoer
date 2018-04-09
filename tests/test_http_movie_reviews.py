bda7e04855afe75699ed30faeb3bce031140a2c4
H. Turgut Uyar <uyar@tekir.org>
2018-04-09 14:35:36 +0300

    refactor movie reviews tests

--- tests/test_http_movie_reviews.py
+++ tests/test_http_movie_reviews.py
@@ -1,3 +1,4 @@
-def test_number_of_reviews_should_be_correct(ia):
-    data = ia.get_movie('0104155', info=['reviews'])    # Dust Devil
-    assert len(data.get('reviews', [])) == 25
+def test_movie_reviews_should_be_a_list(ia):
+    movie = ia.get_movie('0104155', info=['reviews'])   # Dust Devil
+    reviews = movie.get('reviews', [])
+    assert len(reviews) == 25

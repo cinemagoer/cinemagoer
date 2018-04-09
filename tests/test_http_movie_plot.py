5cf34e4a5cf4458bfbb58b90a1119475e8506e81
H. Turgut Uyar <uyar@tekir.org>
2018-04-09 14:31:09 +0300

    refactor movie plot tests

--- tests/test_http_movie_plot.py
+++ tests/test_http_movie_plot.py
@@ -1,16 +1,25 @@
 import re
 
 
-def test_summary_should_end_with_author(ia):
-    data = ia.get_movie('0133093', info=['plot'])   # Matrix
-    assert re.match('^Thomas A\. Anderson is a man .*::redcommander27$', data['plot'][0])
+def test_movie_summary_should_be_some_text_with_author(ia):
+    movie = ia.get_movie('0133093', info=['plot'])  # Matrix
+    plots = movie.get('plot', [])
+    assert len(plots) > 0
+    assert re.match('^Thomas A\. Anderson is a man .* rebellion\.::redcommander27$', plots[0])
 
 
-def test_summary_should_have_synopsis(ia):
-    data = ia.get_movie('0133093', info=['plot'])   # Matrix
-    assert len(data['synopsis']) >= 1 and data['synopsis'][0]
+def test_movie_summary_if_none_should_be_excluded(ia):
+    movie = ia.get_movie('1863157', info=['plot'])  # Ates Parcasi
+    assert 'plot' not in movie
 
 
-def test_summary_none_should_be_excluded(ia):
-    data = ia.get_movie('1863157', info=['plot'])   # Ates Parcasi
-    assert 'plot' not in data
+def test_movie_synopsis_should_be_some_text(ia):
+    movie = ia.get_movie('0133093', info=['plot'])  # Matrix
+    synopsis = movie.get('synopsis')
+    assert len(synopsis) > 0
+    assert re.match('^The screen is filled with .* three Matrix movies\.$', synopsis[0])
+
+
+def test_movie_synopsis_if_none_should_be_excluded(ia):
+    movie = ia.get_movie('1863157', info=['plot'])  # Ates Parcasi
+    assert 'synopsis' not in movie

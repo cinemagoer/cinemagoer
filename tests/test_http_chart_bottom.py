a9d598029966dfde882c52e116e31236739eb22a
H. Turgut Uyar <uyar@tekir.org>
2018-04-09 13:33:14 +0300

    refactor top and bottom chart tests

--- tests/test_http_chart_bottom.py
+++ tests/test_http_chart_bottom.py
@@ -1,15 +1,15 @@
-def test_chart_should_contain_100_movies(ia):
+def test_bottom_chart_should_contain_100_movies(ia):
     data = ia.get_bottom100_movies()
     assert len(data) == 100
 
 
-def test_all_movies_should_have_rating_and_votes(ia):
+def test_bottom_chart_all_movies_should_have_rating_and_votes(ia):
     data = ia.get_bottom100_movies()
     for movie in data:
         assert {'title', 'kind', 'year', 'rating', 'votes'}.issubset(set(movie.keys()))
 
 
-def test_chart_should_contain_manos(ia):
+def test_bottom_chart_should_contain_manos(ia):
     data = ia.get_bottom100_movies()
     movieIDs = [m.movieID for m in data]
     assert '0060666' in movieIDs
--- tests/test_http_chart_top.py
+++ tests/test_http_chart_top.py
@@ -1,27 +1,27 @@
-def test_chart_should_contain_250_movies(ia):
+def test_top_chart_should_contain_250_movies(ia):
     data = ia.get_top250_movies()
     assert len(data) == 250
 
 
-def test_all_movies_should_have_rating_and_votes(ia):
+def test_top_chart_all_movies_should_have_rating_and_votes(ia):
     data = ia.get_top250_movies()
     for movie in data:
         assert {'title', 'kind', 'year', 'rating', 'votes'}.issubset(set(movie.keys()))
 
 
-def test_ranks_should_proceed_in_order(ia):
+def test_top_chart_ranks_should_proceed_in_order(ia):
     data = ia.get_top250_movies()
     ranks = [m['top 250 rank'] for m in data]
     assert ranks == list(range(1, 251))
 
 
-def test_chart_should_contain_matrix(ia):
+def test_top_chart_should_contain_matrix(ia):
     data = ia.get_top250_movies()
     movieIDs = [m.movieID for m in data]
     assert '0133093' in movieIDs
 
 
-def test_shawshank_should_be_top_movie(ia):
+def test_top_chart_shawshank_should_be_top_movie(ia):
     data = ia.get_top250_movies()
     top_movie = data[0]
     assert top_movie.movieID == '0111161'

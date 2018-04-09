67da2e120272faab93ee362bbff62774f85debf0
H. Turgut Uyar <uyar@tekir.org>
2018-04-09 10:34:51 +0300

    rewrite bottom movies tests using access fixture

--- tests/test_http_chart_bottom.py
+++ tests/test_http_chart_bottom.py
@@ -1,34 +1,15 @@
-from pytest import fixture
-
-from imdb.parser.http.topBottomParser import DOMHTMLBottom100Parser
-
-
-@fixture(scope='module')
-def chart_bottom(url_opener, base_url):
-    """A function to retrieve the bottom movies page."""
-    def retrieve():
-        url = base_url + '/chart/bottom'
-        return url_opener.retrieve_unicode(url)
-    return retrieve
-
-
-parser = DOMHTMLBottom100Parser()
-
-
-def test_chart_should_contain_100_movies(chart_bottom):
-    page = chart_bottom()
-    data = parser.parse(page)['data']
+def test_chart_should_contain_100_movies(ia):
+    data = ia.get_bottom100_movies()
     assert len(data) == 100
 
 
-def test_all_movies_should_have_rating_and_votes(chart_bottom):
-    page = chart_bottom()
-    data = parser.parse(page)['data']
-    for movie in dict(data).values():
+def test_all_movies_should_have_rating_and_votes(ia):
+    data = ia.get_bottom100_movies()
+    for movie in data:
         assert {'title', 'kind', 'year', 'rating', 'votes'}.issubset(set(movie.keys()))
 
 
-def test_chart_should_contain_correct_movie(chart_bottom):
-    page = chart_bottom()
-    data = parser.parse(page)['data']
-    assert '0060666' in dict(data)          # Manos
+def test_chart_should_contain_correct_movie(ia):
+    data = ia.get_bottom100_movies()
+    movieIDs = [m.movieID for m in data]
+    assert '0060666' in movieIDs            # Manos

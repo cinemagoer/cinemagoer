eb668da026fe5290e6c5e96c4df111a626672625
H. Turgut Uyar <uyar@tekir.org>
2018-04-09 10:58:14 +0300

    rewrite movie sites tests using access fixture

--- tests/test_http_movie_sites.py
+++ tests/test_http_movie_sites.py
@@ -1,22 +1,4 @@
-from pytest import fixture
-
-from imdb.parser.http.movieParser import DOMHTMLOfficialsitesParser
-
-
-@fixture(scope='module')
-def movie_official_sites(url_opener, movies):
-    """A function to retrieve the official sites page of a test movie."""
-    def retrieve(movie_key):
-        url = movies[movie_key] + '/externalsites'
-        return url_opener.retrieve_unicode(url)
-    return retrieve
-
-
-parser = DOMHTMLOfficialsitesParser()
-
-
-def test_sites_should_be_lists(movie_official_sites):
-    page = movie_official_sites('matrix')
-    data = parser.parse(page)['data']
+def test_sites_should_be_lists(ia):
+    data = ia.get_movie('0133093', info=['official sites'])     # Matrix
     assert len(data.get('official sites', [])) == 1
     assert len(data.get('sound clips', [])) == 3

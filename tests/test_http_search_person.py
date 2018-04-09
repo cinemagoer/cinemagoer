6a6d83f1487663ca72325995f2d6e674056110ae
H. Turgut Uyar <uyar@tekir.org>
2018-04-09 13:27:53 +0300

    rewrite person search tests using access fixture

--- tests/test_http_search_person.py
+++ tests/test_http_search_person.py
@@ -1,22 +1,7 @@
-from pytest import fixture, mark
-
-from imdb.parser.http.searchPersonParser import DOMHTMLSearchPersonParser
-
-
-@fixture(scope='module')
-def search_person(url_opener, search):
-    """A function to retrieve the search result for a person."""
-    def retrieve(term):
-        url = search + '?s=nm&q=' + term.replace(' ', '+')
-        return url_opener.retrieve_unicode(url)
-    return retrieve
-
-
-parser = DOMHTMLSearchPersonParser()
+from pytest import mark
 
 
 @mark.fragile
-def test_found_many_result_should_contain_correct_number_of_people(search_person):
-    page = search_person('keanu reeves')
-    data = parser.parse(page)['data']
+def test_found_many_result_should_contain_correct_number_of_people(ia):
+    data = ia.search_person('keanu reeves')
     assert len(data) == 4

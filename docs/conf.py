3cfeb4c50f45749fdb92bf3c636925c191ae4d95
H. Turgut Uyar <uyar@tekir.org>
2018-06-17 18:54:06 +0300

    update docs version to 6.6

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'Davide Alberani, H. Turgut Uyar'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '6.5'
+release = '6.6'
 
 
 # -- General configuration ---------------------------------------------------

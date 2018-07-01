c5aff6a2b56cba49d1e455bdc26d23ac8dfd7b93
H. Turgut Uyar <uyar@tekir.org>
2018-07-01 18:31:20 +0300

    add Python 3.7 to supported versions

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ Natural Language :: English
 Natural Language :: Italian
 Natural Language :: Turkish
 Programming Language :: Python
-Programming Language :: Python :: 3.6
+Programming Language :: Python :: 3.7
 Programming Language :: Python :: 3.5
 Programming Language :: Python :: 3.4
 Programming Language :: Python :: 2.7

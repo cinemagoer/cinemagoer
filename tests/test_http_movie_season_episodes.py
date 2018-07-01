1cb6092c4e8f63cd944586d965a4b74fa9a419b1
H. Turgut Uyar <uyar@tekir.org>
2018-07-01 18:29:34 +0300

    force sort order in season tests

--- tests/test_http_movie_season_episodes.py
+++ tests/test_http_movie_season_episodes.py
@@ -3,12 +3,12 @@ from pytest import mark
 
 def test_series_episodes_should_be_a_map_of_seasons_and_episodes(ia):
     movie = ia.get_movie('0412142', info=['episodes'])      # House M.D.
-    assert list(movie.get('episodes')) == list(range(1, 9))
+    assert list(sorted(movie.get('episodes'))) == list(range(1, 9))
 
 
 def test_series_episodes_with_unknown_season_should_have_placeholder_at_end(ia):
     movie = ia.get_movie('0436992', info=['episodes'])      # Doctor Who
-    assert list(movie.get('episodes')) == list(range(1, 12)) + [-1]
+    assert list(sorted(movie.get('episodes'))) == [-1] + list(range(1, 12))
 
 
 @mark.skip('episodes is {} instead of None')

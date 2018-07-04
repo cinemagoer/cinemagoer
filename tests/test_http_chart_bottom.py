624bcf4110855b046586e7cbb1421bb95b2494fe
H. Turgut Uyar <uyar@tekir.org>
2018-07-04 11:46:27 +0300

    refactor top/bottom chart tests

--- tests/test_http_chart_bottom.py
+++ tests/test_http_chart_bottom.py
@@ -1,49 +1,38 @@
-from imdb.Movie import Movie
-
-
-chart_keys = {'bottom 100 rank', 'title', 'kind', 'year', 'rating', 'votes'}
-
-
 def test_bottom_chart_should_contain_100_entries(ia):
     chart = ia.get_bottom100_movies()
     assert len(chart) == 100
 
 
-def test_bottom_chart_all_entries_should_be_movies(ia):
-    chart = ia.get_bottom100_movies()
-    for movie in chart:
-        assert isinstance(movie, Movie)
+def test_bottom_chart_entries_should_have_rank(ia):
+    movies = ia.get_bottom100_movies()
+    assert movies[0]['bottom 100 rank'] == 1
 
 
-def test_bottom_chart_all_movies_should_have_movie_ids(ia):
-    chart = ia.get_bottom100_movies()
-    for movie in chart:
-        assert hasattr(movie, 'movieID')
+def test_bottom_chart_entries_should_have_movie_id(ia):
+    movies = ia.get_bottom100_movies()
+    assert movies[0].movieID == '4458206'
 
 
-def test_bottom_chart_all_movies_should_have_same_keys(ia):
-    chart = ia.get_bottom100_movies()
-    for movie in chart:
-        assert chart_keys.issubset(set(movie.keys()))
+def test_bottom_chart_entries_should_have_title(ia):
+    movies = ia.get_bottom100_movies()
+    assert movies[0]['title'] == 'Code Name: K.O.Z.'
 
 
-def test_bottom_chart_ranks_should_proceed_in_order(ia):
-    chart = ia.get_bottom100_movies()
-    ranks = [m['bottom 100 rank'] for m in chart]
-    assert ranks == list(range(1, 101))
+def test_bottom_chart_entries_should_have_kind(ia):
+    movies = ia.get_bottom100_movies()
+    assert movies[0]['kind'] == 'movie'
 
 
-def test_bottom_chart_should_contain_manos(ia):
-    chart = ia.get_bottom100_movies()
-    movieIDs = [m.movieID for m in chart]
-    assert '0060666' in movieIDs
+def test_bottom_chart_entries_should_have_year(ia):
+    movies = ia.get_bottom100_movies()
+    assert movies[0]['year'] == 2015
 
 
-def test_bottom_chart_koz_should_be_bottom_movie(ia):
-    movie = ia.get_bottom100_movies()[0]
-    assert movie.movieID == '4458206'
-    assert movie['title'] == 'Code Name: K.O.Z.'
-    assert movie['kind'] == 'movie'
-    assert movie['year'] == 2015
-    assert movie['rating'] < 1.6
-    assert movie['votes'] > 25000
+def test_bottom_chart_entries_should_have_rating(ia):
+    movies = ia.get_bottom100_movies()
+    assert movies[0]['rating'] < 1.6
+
+
+def test_bottom_chart_entries_should_have_votes(ia):
+    movies = ia.get_bottom100_movies()
+    assert movies[0]['votes'] > 25000
--- tests/test_http_chart_top.py
+++ tests/test_http_chart_top.py
@@ -1,49 +1,38 @@
-from imdb.Movie import Movie
-
-
-chart_keys = {'top 250 rank', 'title', 'kind', 'year', 'rating', 'votes'}
-
-
 def test_top_chart_should_contain_250_entries(ia):
     chart = ia.get_top250_movies()
     assert len(chart) == 250
 
 
-def test_top_chart_all_entries_should_be_movies(ia):
-    chart = ia.get_top250_movies()
-    for movie in chart:
-        assert isinstance(movie, Movie)
+def test_top_chart_entries_should_have_rank(ia):
+    movies = ia.get_top250_movies()
+    assert movies[0]['top 250 rank'] == 1
 
 
-def test_top_chart_all_movies_should_have_movie_ids(ia):
-    chart = ia.get_top250_movies()
-    for movie in chart:
-        assert hasattr(movie, 'movieID')
+def test_top_chart_entries_should_have_movie_id(ia):
+    movies = ia.get_top250_movies()
+    assert movies[0].movieID == '0111161'
 
 
-def test_top_chart_all_movies_should_have_same_keys(ia):
-    chart = ia.get_top250_movies()
-    for movie in chart:
-        assert chart_keys.issubset(set(movie.keys()))
+def test_top_chart_entries_should_have_title(ia):
+    movies = ia.get_top250_movies()
+    assert movies[0]['title'] == 'The Shawshank Redemption'
 
 
-def test_top_chart_ranks_should_proceed_in_order(ia):
-    chart = ia.get_top250_movies()
-    ranks = [m['top 250 rank'] for m in chart]
-    assert ranks == list(range(1, 251))
+def test_top_chart_entries_should_have_kind(ia):
+    movies = ia.get_top250_movies()
+    assert movies[0]['kind'] == 'movie'
 
 
-def test_top_chart_should_contain_matrix(ia):
-    chart = ia.get_top250_movies()
-    movieIDs = [m.movieID for m in chart]
-    assert '0133093' in movieIDs
+def test_top_chart_entries_should_have_year(ia):
+    movies = ia.get_top250_movies()
+    assert movies[0]['year'] == 1994
 
 
-def test_top_chart_shawshank_should_be_top_movie(ia):
-    movie = ia.get_top250_movies()[0]
-    assert movie.movieID == '0111161'
-    assert movie['title'] == 'The Shawshank Redemption'
-    assert movie['kind'] == 'movie'
-    assert movie['year'] == 1994
-    assert movie['rating'] > 9
-    assert movie['votes'] > 1900000
+def test_top_chart_entries_should_have_rating(ia):
+    movies = ia.get_top250_movies()
+    assert movies[0]['rating'] > 9
+
+
+def test_top_chart_entries_should_have_votes(ia):
+    movies = ia.get_top250_movies()
+    assert movies[0]['votes'] > 1900000

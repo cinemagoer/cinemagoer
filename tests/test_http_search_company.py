27a7029881268637b4e727ec438c9b8848cc0e83
Davide Alberani <da@erlug.linux.it>
2018-08-04 11:35:34 +0200

    made test_http_search_company more robust

--- tests/test_http_search_company.py
+++ tests/test_http_search_company.py
@@ -15,7 +15,7 @@ def test_search_company_unlimited_should_list_correct_number_of_companies(ia):
 
 def test_search_company_too_many_should_list_upper_limit_of_companies(ia):
     companies = ia.search_company('pictures', results=500)
-    assert len(companies) == 200
+    assert len(companies) >= 199
 
 
 def test_search_company_if_none_result_should_be_empty(ia):

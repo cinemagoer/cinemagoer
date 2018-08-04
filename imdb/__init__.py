ab598516f7adfba9477d0bd6aa28c9240cc419d2
Davide Alberani <da@erlug.linux.it>
2018-08-04 20:24:40 +0200

    bump version

--- imdb/__init__.py
+++ imdb/__init__.py
@@ -25,7 +25,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 __all__ = ['IMDb', 'IMDbError', 'Movie', 'Person', 'Character', 'Company',
            'available_access_systems']
-__version__ = VERSION = '6.6dev20180729b'
+__version__ = VERSION = '6.6dev20180804'
 
 
 import logging

08d46fdff8841f3c0aa6137b41ff601fede89c38
Davide Alberani <da@erlug.linux.it>
2018-10-20 20:30:45 +0200

    fix test_movie_summary_should_be_some_text_with_author

--- tests/test_http_movie_plot.py
+++ tests/test_http_movie_plot.py
@@ -5,7 +5,12 @@ def test_movie_summary_should_be_some_text_with_author(ia):
     movie = ia.get_movie('0133093', info=['plot'])  # Matrix
     plots = movie.get('plot', [])
     assert 5 <= len(plots) <= 10
-    assert re.match('^Thomas A\. Anderson is a man .* rebellion\.::redcommander27$', plots[0])
+    kc_plot = ''
+    for plot in plots:
+        if plot.endswith('Kenneth Chisholm'):
+            kc_plot = plot
+            break
+    assert re.match('^A computer hacker .*controllers\.::Kenneth Chisholm$', kc_plot)
 
 
 def test_movie_summary_if_none_should_be_excluded(ia):

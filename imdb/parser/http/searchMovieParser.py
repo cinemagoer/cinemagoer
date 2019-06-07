b7bfc6cbc120797aeb6fd83635353041d7113d43
Charlotte Weaver <charlottesweaver@gmail.com>
2019-06-06 22:07:30 -0700

    grab poster from search

--- imdb/parser/http/searchMovieParser.py
+++ imdb/parser/http/searchMovieParser.py
@@ -40,25 +40,30 @@ class DOMHTMLSearchMovieParser(DOMParserBase):
         Rule(
             key='data',
             extractor=Rules(
-                foreach='//td[@class="result_text"]',
+                foreach="//tr[contains(concat(' ',normalize-space(@class),' '),' findResult ')]",
                 rules=[
                     Rule(
                         key='link',
-                        extractor=Path('./a/@href', reduce=reducers.first)
+                        extractor=Path('./td[@class="result_text"]/a/@href', reduce=reducers.first)
                     ),
                     Rule(
                         key='info',
-                        extractor=Path('.//text()')
+                        extractor=Path('./td[@class="result_text"]//text()')
                     ),
                     Rule(
                         key='akas',
-                        extractor=Path(foreach='./i', path='./text()')
+                        extractor=Path(foreach='./td[@class="result_text"]/i', path='./text()')
+                    ),
+                    Rule(
+                        key='cover url',
+                        extractor=Path('./td[@class="primary_photo"]/a/img/@src')
                     )
                 ],
                 transform=lambda x: (
                     analyze_imdbid(x.get('link')),
                     analyze_title(x.get('info', '')),
-                    x.get('akas')
+                    x.get('akas'),
+                    x.get('cover url')
                 )
             )
         )
@@ -78,7 +83,7 @@ class DOMHTMLSearchMovieParser(DOMParserBase):
             data['data'][:] = data['data'][:results]
         # Horrible hack to support AKAs.
             data['data'] = [x for x in data['data'] if x[0] and x[1]]
-        if data and data['data'] and len(data['data'][0]) == 3 and isinstance(data['data'][0], tuple):
+        if data and data['data'] and len(data['data'][0]) == 4 and isinstance(data['data'][0], tuple):
             for idx, datum in enumerate(data['data']):
                 if not isinstance(datum, tuple):
                     continue
@@ -87,9 +92,9 @@ class DOMHTMLSearchMovieParser(DOMParserBase):
                 if datum[2] is not None:
                     akas = [aka[1:-1] for aka in datum[2]]  # remove the quotes
                     datum[1]['akas'] = akas
-                    data['data'][idx] = (datum[0], datum[1])
-                else:
-                    data['data'][idx] = (datum[0], datum[1])
+                if datum[3] is not None:
+                    datum[1]['cover url'] = datum[3]
+                data['data'][idx] = (datum[0], datum[1])
         return data
 
     def add_refs(self, data):

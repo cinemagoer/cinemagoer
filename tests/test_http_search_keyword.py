b5e58041df7cbf4109b2d0b907c9eece66a862fc
Ted Spence <tspence@fb.com>
2019-08-21 19:19:47 -0700

    Add test

--- tests/test_http_search_keyword.py
+++ tests/test_http_search_keyword.py
@@ -16,3 +16,14 @@ def test_search_keyword_if_too_many_should_list_upper_limit_of_keywords(ia):
 def test_search_keyword_if_none_result_should_be_empty(ia):
     keywords = ia.search_keyword('%e3%82%a2')
     assert keywords == []
+
+
+def test_get_keyword_pagination(ia):
+    superheroes_without_page_param = ia.get_keyword('superhero')
+    superheroes_page_one = ia.get_keyword('superhero', page=1)
+    superheroes_page_two = ia.get_keyword('superhero', page=2)
+    for i in range(50):
+        assert superheroes_without_page_param[i]['title'] == superheroes_page_one[i]['title']
+        assert superheroes_without_page_param[i]['title'] != superheroes_page_two[i]['title']
+
+    

aab4cf9fb5a053890f4e8afac73fbbffe42f40bf
Evangelos Foutras <evangelos@foutrelis.com>
2020-11-27 13:44:42 +0200

    msgfmt.py: use array.tobytes() on Python >= 3.2
    array.tostring() was deprecated in Python 3.2 and removed in 3.9.

--- imdb/locale/msgfmt.py
+++ imdb/locale/msgfmt.py
@@ -87,7 +87,10 @@ def generate():
                          7*4,               # start of key index
                          7*4+len(keys)*8,   # start of value index
                          0, 0)              # size and offset of hash table
-    output += array.array("i", offsets).tostring()
+    if sys.version_info < (3, 2):
+        output += array.array("i", offsets).tostring()
+    else:
+        output += array.array("i", offsets).tobytes()
     output += ids
     output += strs
     return output

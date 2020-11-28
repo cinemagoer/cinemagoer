c3a4c7253cac2bb4bb66ad050b9f829a2645d68e
Davide Alberani <da@erlug.linux.it>
2020-11-28 17:46:26 +0100

    update test suite

--- tests/test_http_movie_sites.py
+++ tests/test_http_movie_sites.py
@@ -1,7 +1,7 @@
 def test_movie_official_sites_should_be_a_list(ia):
     movie = ia.get_movie('0133093', info=['official sites'])    # Matrix
     official_sites = movie.get('official sites', [])
-    assert len(official_sites) == 2
+    assert len(official_sites) == 3
 
 
 def test_movie_official_sites_if_none_should_be_excluded(ia):

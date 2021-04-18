eccdcf143f966f9c4046e073a7be85a9a98759da
Davide Alberani <da@erlug.linux.it>
2021-04-18 14:05:18 +0200

    skip localized title test

--- tests/test_http_movie_titles.py
+++ tests/test_http_movie_titles.py
@@ -1,7 +1,10 @@
+from pytest import mark
+
+@mark.skip('obviously this depends on your country of origin')
 def test_movie_localized_title(ia):
     movie = ia.get_movie('2991224', info=['main'])
     title = movie.get('localized title', '')
-    assert title == 'Tangerines - Mandarini'
+    assert title in ('Tangerines - Mandarini', 'Tangerines')
 
 
 def test_movie_original_title(ia):
